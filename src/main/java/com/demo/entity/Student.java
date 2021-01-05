7e3ace138ec7a5196e5e9a65ae737c103ac50541
smali-bang <2034980072@qq.com>
2021-01-05 15:08:52 +0800

    ces

---
+++ src/main/java/com/demo/entity/Student.java
@@ -0,0 +1,11 @@
+package com.demo.entity;/*
+ * User:龙婷杰
+ * Date:2021/1/5
+ * Time:15:05
+ */
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+}
